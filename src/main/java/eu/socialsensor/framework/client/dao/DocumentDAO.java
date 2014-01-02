4e5c4730dccd9a28d1afed4b3141e3c184988fb2
matzika <katerina.iliakopoulou@gmail.com>
2014-01-02 12:02:58 +0200

    Interface DocumentDAO added

---
+++ src/main/java/eu/socialsensor/framework/client/dao/DocumentDAO.java
@@ -0,0 +1,18 @@
+package eu.socialsensor.framework.client.dao;
+
+import java.util.List;
+
+import eu.socialsensor.framework.common.domain.Document;
+
+public interface DocumentDAO {
+	
+	public void insertDocument(Document document);
+	
+	public void updateDocument(Document document);
+	
+	public boolean deleteDocument(String id);
+	
+	public Document getDocument(String id);
+	
+	public List<Document> getDocumentsSince(long date);
+}
