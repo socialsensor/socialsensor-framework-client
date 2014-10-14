ceb8753172fdde557eb1ac2af833dfeb7a1e9f52
manosetro <manosetro@iti.gr>
2014-10-14 13:54:38 +0300

    Fix Media Searcher Client

--- src/main/java/eu/socialsensor/framework/client/search/MediaSearcher.java
+++ src/main/java/eu/socialsensor/framework/client/search/MediaSearcher.java
@@ -9,12 +9,18 @@ import redis.clients.jedis.JedisPoolConfig;
 public class MediaSearcher {
 
 	public final Logger logger = Logger.getLogger(MediaSearcher.class);
-	private String searcherHostname;
 	public static final String CHANNEL = "searchRequestsChannel";
 	
+	private String searcherHostname;
+	
+	private JedisPool jedisPool;
 	
 	public MediaSearcher(String searcherHostname) {
 		this.searcherHostname = searcherHostname;
+		
+		JedisPoolConfig poolConfig = new JedisPoolConfig();
+		
+        this.jedisPool = new JedisPool(poolConfig, this.searcherHostname, 6379, 0);
 	}
 	
 	public void search(String message) {
@@ -28,20 +34,19 @@ public class MediaSearcher {
 	public void search(String message, String channel) {
 		Jedis publisherJedis = null;
 		try {
-			JedisPoolConfig poolConfig = new JedisPoolConfig();
-	        JedisPool jedisPool = new JedisPool(poolConfig, searcherHostname, 6379, 0);
-	        
 	        publisherJedis = jedisPool.getResource();
-	        
 			publisherJedis.publish(channel, message);
+
 		}
 		catch(Exception e) {
 			e.printStackTrace();
 			logger.error(e);
 		}
 		finally {
-			if(publisherJedis != null)
+			if(publisherJedis != null) {
 				publisherJedis.disconnect();
+				jedisPool.returnResource(publisherJedis);
+			}
 		}
 	}
 	
@@ -49,11 +54,7 @@ public class MediaSearcher {
 		
 		Jedis publisherJedis = null;
 		try {
-			JedisPoolConfig poolConfig = new JedisPoolConfig();
-	        JedisPool jedisPool = new JedisPool(poolConfig, searcherHostname, 6379, 0);
-	        
 	        publisherJedis = jedisPool.getResource();
-	        
 			publisherJedis.publish(channel, message);
 		}
 		catch(Exception e) {
@@ -61,9 +62,23 @@ public class MediaSearcher {
 			logger.error(e);
 		}
 		finally {
-			if(publisherJedis != null)
+			if(publisherJedis != null) {
 				publisherJedis.disconnect();
+				jedisPool.returnResource(publisherJedis);
+			}
 		}
 	}
 	
+	public static void main(String...args) {
+		MediaSearcher searcher = new MediaSearcher("160.40.51.18");
+		
+		for(int i=0; i<10; i++) {
+			searcher.search("test_"+i, "test");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
